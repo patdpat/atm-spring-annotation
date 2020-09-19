6fda55256ae8155ea4a4fa57c56580fece5c16d6
sidtipat.k <s.kietchai@outlook.com>
2020-09-19 22:51:37 +0700

    add annotation

--- src/main/java/atm/DataSourceFile.java
+++ src/main/java/atm/DataSourceFile.java
@@ -1,11 +1,17 @@
 package atm;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
+@Primary
+@Component
 public class DataSourceFile implements DataSource {
 
     private String filename;
@@ -13,7 +19,7 @@ public class DataSourceFile implements DataSource {
     /**
      * @param filename the name of the customer file
      */
-    public DataSourceFile(String filename) {
+    public DataSourceFile(@Value("${filename}") String filename) {
         this.filename = filename;
     }
 
@@ -21,15 +27,15 @@ public class DataSourceFile implements DataSource {
      * Reads the customer numbers and pins
      * and initializes the bank accounts.
      */
-    public Map<Integer,Customer> readCustomers() {
+    public Map<Integer, Customer> readCustomers() {
 
-        Map<Integer,Customer> customers = new HashMap<>();
+        Map<Integer, Customer> customers = new HashMap<>();
         Scanner in;
 
         try {
             in = new Scanner(new FileReader(filename));
         } catch (FileNotFoundException e) {
-            System.err.println("Problem reading a customer file: "+filename);
+            System.err.println("Problem reading a customer file: " + filename);
             return customers;
         }
 
--- src/main/java/atm/Main.java
+++ src/main/java/atm/Main.java
@@ -7,9 +7,19 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
-//      CodeConfig-based
+//        DataSourceDB dataSourceDB = new DataSourceDB();
+//        DataSourceFile dataSourceFile = new DataSourceFile("customers.txt");
+//        Bank bank = new Bank("My Bank", dataSourceFile);
+//        ATM atm = new ATM(bank);
+//        AtmUI atmUI = new AtmUI(atm);
+//        atmUI.run();
+
+        // CodeConfig-based
+//        ApplicationContext context =
+//                new AnnotationConfigApplicationContext(ATMConfig.class);
+        // Annotation-based
         ApplicationContext context =
-                new AnnotationConfigApplicationContext(ATMConfig.class);
+                new ClassPathXmlApplicationContext("bean.xml");
 
         AtmUI atmUI = context.getBean(AtmUI.class);
         atmUI.run();
